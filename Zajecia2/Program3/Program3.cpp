9399b9c30883788d5fcaa293d001c17d552cbd6b
JustinaSofia <jutadulce678@amorki.pl>
2017-04-03 19:41:15 +0200

    Zajecia2

---
+++ Zajecia2/Program3/Program3.cpp
@@ -0,0 +1,272 @@
+// Program3.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include "math.h"
+#include <conio.h>
+#ifdef __APPLE__
+#include <GLUT/glut.h>
+#else
+#include <GL/glut.h>
+#endif
+
+#include "gltools_extracted.h"
+
+GLfloat gltDistanceToPlane(GLTVector3 vPoint, GLTVector4 vPlane);
+void gltGetPlaneEquation(GLTVector3 vPoint1, GLTVector3 vPoint2, GLTVector3 vPoint3, GLTVector3 vPlane);
+void gltRotateVector(const GLTVector3 vSrcVector, const GLTMatrix mMatrix, GLTVector3 vOut);
+void gltTransformPoint(const GLTVector3 vSrcVector, const GLTMatrix mMatrix, GLTVector3 vOut);
+void gltGetNormalVector(const GLTVector3 vP1, const GLTVector3 vP2, const GLTVector3 vP3, GLTVector3 vNormal);
+void gltVectorCrossProduct(const GLTVector3 vU, const GLTVector3 vV, GLTVector3 vResult);
+GLfloat gltVectorDotProduct(const GLTVector3 vU, const GLTVector3 vV);
+void gltCopyVector(const GLTVector3 vSource, GLTVector3 vDest);
+void gltNormalizeVector(GLTVector3 vNormal);
+GLfloat gltGetVectorLength(const GLTVector3 vVector);
+GLfloat gltGetVectorLengthSqrd(const GLTVector3 vVector);
+void gltScaleVector(GLTVector3 vVector, const GLfloat fScale);
+void gltSubtractVectors(const GLTVector3 vFirst, const GLTVector3 vSecond, GLTVector3 vResult);
+void gltAddVectors(const GLTVector3 vFirst, const GLTVector3 vSecond, GLTVector3 vResult);
+
+static GLfloat xRot = 0.0f;
+static GLfloat yRot = 0.0f;
+const GLfloat GL_PI = 3.1415f;
+
+
+void ChangeSize(int w, int h)
+{
+	GLfloat fAspect;
+
+	if (h == 0)
+		h = 1;
+	glViewport(0, 0, w, h);
+	fAspect = (GLfloat)w / (GLfloat)h;
+	glMatrixMode(GL_PROJECTION);
+	glLoadIdentity();
+	gluPerspective(35.0f, fAspect, 1.0, 40.0);
+	glMatrixMode(GL_MODELVIEW);
+	glLoadIdentity();
+}
+
+void SetupRC()
+{
+	GLuint texture;
+	int x, y;
+	char pixels[256 * 256];
+	GLint iWidth = 256;
+	GLint iHeight = 256;
+	GLint iComponents = GL_RGB;
+	GLenum eFormat = {
+		GL_RGBA
+	};
+
+	GLfloat whiteLight[] = { 0.05f, 0.05f, 0.05f, 1.0f };
+	GLfloat sourceLight[] = { 0.25f, 0.25f, 0.25f, 1.0f };
+	GLfloat lightPos[] = { -10.f, 5.0f, 5.0f, 1.0f };
+	glEnable(GL_DEPTH_TEST);
+	glEnable(GL_CULL_FACE);
+	glFrontFace(GL_CCW);
+
+	glEnable(GL_LIGHTING);
+	glLightModelfv(GL_LIGHT_MODEL_AMBIENT, whiteLight);
+	glLightfv(GL_LIGHT0, GL_AMBIENT, sourceLight);
+	glLightfv(GL_LIGHT0, GL_DIFFUSE, sourceLight);
+	glLightfv(GL_LIGHT0, GL_POSITION, lightPos);
+	glEnable(GL_LIGHT0);
+
+	glEnable(GL_COLOR_MATERIAL);
+	glColorMaterial(GL_FRONT, GL_AMBIENT_AND_DIFFUSE);
+	glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+	glPixelStorei(GL_UNPACK_ALIGNMENT, 1);
+	glGenTextures(1, &texture);
+	glBindTexture(GL_TEXTURE_2D, texture);
+
+
+	for (y = 0; y < 256; y++)
+	{
+		for (x = 0; x < 256; x++)
+			pixels[y * 256 + x] = rand() % 256;
+	}
+
+	glTexImage2D(GL_TEXTURE_2D, 0, GL_LUMINANCE, 256, 256, 0, GL_LUMINANCE, GL_UNSIGNED_BYTE, pixels);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+	glTexEnvi(GL_TEXTURE_ENV, GL_TEXTURE_ENV_MODE, GL_MODULATE);
+	glEnable(GL_TEXTURE_2D);
+}
+
+void SpecialKeys(int key, int x, int y)
+{
+	if (key == GLUT_KEY_UP)
+		xRot -= 5.0f;
+	if (key == GLUT_KEY_DOWN)
+		xRot += 5.0f;
+	if (key == GLUT_KEY_LEFT)
+		yRot -= 5.0f;
+	if (key == GLUT_KEY_RIGHT)
+		yRot += 5.0f;
+	xRot = (GLfloat)((const int)xRot % 360);
+	yRot = (GLfloat)((const int)yRot % 360);
+
+	glutPostRedisplay();
+}
+
+void RenderScene(void)
+{
+	const GLint N = 7;
+	GLTVector3 vNormal;
+	GLTVector3 vCorners[N];
+	GLTVector3 topCorner{ 0.0f, 1.2f, 0.0f };
+	GLTVector3 center{ 0.0f, 0.0f, 0.0f };
+	for (int i = 0; i < N; ++i)
+	{
+		const GLfloat kat = (GLfloat)i / N * GL_PI * 2.0;
+		const GLfloat x = sin(kat);
+		const GLfloat y = cos(kat);
+
+		vCorners[i][0] = x;
+		vCorners[i][1] = 0.0f;
+		vCorners[i][2] = y;
+	}
+
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+	glPushMatrix();
+
+	glTranslatef(0.0f, -0.25f, -4.0f);
+	glRotatef(xRot, 1.0f, 0.0f, 0.0f);
+	glRotatef(yRot, 0.0f, 1.0f, 0.0f);
+
+	glColor3f(1.0f, 1.0f, 1.0f);
+	glBegin(GL_TRIANGLES);
+
+	glNormal3f(0.0f, -1.0f, 0.0f);
+	for (int i = 0; i < N; ++i)
+	{
+		glTexCoord2f(0.0f, 1.0f);
+		glVertex3fv(center);
+		glTexCoord2f(0.0f, 0.0f);
+		glVertex3fv(vCorners[(i + 1) % N]);
+		glTexCoord2f(1.0f, 1.0f);
+		glVertex3fv(vCorners[i]);
+	}
+
+	GLTVector3 normal;
+	for (int i = 0; i < N; i++)
+	{
+		gltGetNormalVector(vCorners[i], vCorners[(i + 1) % N], topCorner, normal);
+		glNormal3fv(normal);
+		glTexCoord2f(1.0f, 1.0f);
+		glVertex3fv(vCorners[i]);
+		glTexCoord2f(0.0f, 0.0f);
+		glVertex3fv(vCorners[(i + 1) % N]);
+		glTexCoord2f(0.0f, 1.0f);
+		glVertex3fv(topCorner);
+	}
+
+	glEnd();
+
+	glPopMatrix();
+
+	glutSwapBuffers();
+}
+int main(int argc, char *argv[])
+{
+	glutInit(&argc, argv);
+	glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB | GLUT_DEPTH);
+	glutInitWindowSize(800, 600);
+	glutCreateWindow("Textured Pyramid");
+	glutReshapeFunc(ChangeSize);
+	glutSpecialFunc(SpecialKeys);
+	glutDisplayFunc(RenderScene);
+	SetupRC();
+	glutMainLoop();
+	return 0;
+}
+
+void gltAddVectors(const GLTVector3 vFirst, const GLTVector3 vSecond, GLTVector3 vResult) {
+	vResult[0] = vFirst[0] + vSecond[0];
+	vResult[1] = vFirst[1] + vSecond[1];
+	vResult[2] = vFirst[2] + vSecond[2];
+}
+
+void gltSubtractVectors(const GLTVector3 vFirst, const GLTVector3 vSecond, GLTVector3 vResult)
+{
+	vResult[0] = vFirst[0] - vSecond[0];
+	vResult[1] = vFirst[1] - vSecond[1];
+	vResult[2] = vFirst[2] - vSecond[2];
+}
+
+void gltScaleVector(GLTVector3 vVector, const GLfloat fScale)
+{
+	vVector[0] *= fScale; vVector[1] *= fScale; vVector[2] *= fScale;
+}
+
+GLfloat gltGetVectorLengthSqrd(const GLTVector3 vVector)
+{
+	return (vVector[0] * vVector[0]) + (vVector[1] * vVector[1]) + (vVector[2] * vVector[2]);
+}
+
+GLfloat gltGetVectorLength(const GLTVector3 vVector)
+{
+	return (GLfloat)sqrt(gltGetVectorLengthSqrd(vVector));
+}
+
+void gltNormalizeVector(GLTVector3 vNormal)
+{
+	GLfloat fLength = 1.0f / gltGetVectorLength(vNormal);
+	gltScaleVector(vNormal, fLength);
+}
+
+void gltCopyVector(const GLTVector3 vSource, GLTVector3 vDest)
+{
+	memcpy(vDest, vSource, sizeof(GLTVector3));
+}
+
+GLfloat gltVectorDotProduct(const GLTVector3 vU, const GLTVector3 vV)
+{
+	return vU[0] * vV[0] + vU[1] * vV[1] + vU[2] * vV[2];
+}
+
+void gltVectorCrossProduct(const GLTVector3 vU, const GLTVector3 vV, GLTVector3 vResult)
+{
+	vResult[0] = vU[1] * vV[2] - vV[1] * vU[2];
+	vResult[1] = -vU[0] * vV[2] + vV[0] * vU[2];
+	vResult[2] = vU[0] * vV[1] - vV[0] * vU[1];
+}
+
+void gltGetNormalVector(const GLTVector3 vP1, const GLTVector3 vP2, const GLTVector3 vP3, GLTVector3 vNormal)
+{
+	GLTVector3 vV1, vV2;
+
+	gltSubtractVectors(vP2, vP1, vV1);
+	gltSubtractVectors(vP3, vP1, vV2);
+
+	gltVectorCrossProduct(vV1, vV2, vNormal);
+	gltNormalizeVector(vNormal);
+}
+
+void gltTransformPoint(const GLTVector3 vSrcVector, const GLTMatrix mMatrix, GLTVector3 vOut)
+{
+	vOut[0] = mMatrix[0] * vSrcVector[0] + mMatrix[4] * vSrcVector[1] + mMatrix[8] * vSrcVector[2] + mMatrix[12];
+	vOut[1] = mMatrix[1] * vSrcVector[0] + mMatrix[5] * vSrcVector[1] + mMatrix[9] * vSrcVector[2] + mMatrix[13];
+	vOut[2] = mMatrix[2] * vSrcVector[0] + mMatrix[6] * vSrcVector[1] + mMatrix[10] * vSrcVector[2] + mMatrix[14];
+}
+
+void gltRotateVector(const GLTVector3 vSrcVector, const GLTMatrix mMatrix, GLTVector3 vOut)
+{
+	vOut[0] = mMatrix[0] * vSrcVector[0] + mMatrix[4] * vSrcVector[1] + mMatrix[8] * vSrcVector[2];
+	vOut[1] = mMatrix[1] * vSrcVector[0] + mMatrix[5] * vSrcVector[1] + mMatrix[9] * vSrcVector[2];
+	vOut[2] = mMatrix[2] * vSrcVector[0] + mMatrix[6] * vSrcVector[1] + mMatrix[10] * vSrcVector[2];
+}
+
+void gltGetPlaneEquation(GLTVector3 vPoint1, GLTVector3 vPoint2, GLTVector3 vPoint3, GLTVector3 vPlane)
+{
+	gltGetNormalVector(vPoint1, vPoint2, vPoint3, vPlane);
+	vPlane[3] = -(vPlane[0] * vPoint3[0] + vPlane[1] * vPoint3[1] + vPlane[2] * vPoint3[2]);
+}
+
+GLfloat gltDistanceToPlane(GLTVector3 vPoint, GLTVector4 vPlane)
+{
+	return vPoint[0] * vPlane[0] + vPoint[1] * vPlane[1] + vPoint[2] * vPlane[2] + vPlane[3];
+}
+
